e6535429d47339373cc2e16190802d588f922803
Qondzio <qondziolabaziewicz@gmail.com>
2025-03-05 22:54:37 +0100

    Finished 'Rock, paper, scissors' game.

---
+++ script.js
@@ -0,0 +1,119 @@
+let playerChoice 
+let randomNumber
+let computerChoice
+let playerPoints=0;
+let computerPoints=0;
+
+
+alert("Welcome to the 'Rock, paper, scissors' game!")
+playRound()
+
+function playRound() {
+        playerChoice = prompt("Choose 'rock', 'paper' or 'scissors'.");
+        getComputerChoice();
+
+        switch (playerChoice) {
+            case "rock":
+                if (computerChoice==="rock") 
+                    {
+                        draw();
+                    }
+                else if(computerChoice==="paper")
+                    {
+                        computerWins();
+                    }
+                else
+                    {
+                        playerWins();
+                    }
+            break;
+
+            case "paper":
+                if (computerChoice=="rock") 
+                    {
+                        playerWins();
+                    }
+                else if(computerChoice==="paper")
+                    {
+                        draw();
+                    }
+                else
+                    {
+                        computerWins();
+                    }
+            break;
+            
+            case "scissors":
+                if (computerChoice==="rock") 
+                    {
+                        computerWins();
+                    }
+                else if(computerChoice==="paper")
+                    {
+                        playerWins();
+                    }
+                else
+                    {
+                        draw();
+                    }
+            break;
+        }
+    gameOver();
+}
+
+function computerWins()
+{
+    computerPoints++;
+    console.log("Computer wins.");
+    console.log(playerPoints);
+    console.log(computerPoints);
+}
+
+function playerWins()
+{
+    playerPoints++;
+    console.log("You win.");
+    console.log(playerPoints);
+    console.log(computerPoints);
+}
+
+function draw()
+{
+    console.log("Draw.");
+    console.log(playerPoints);
+    console.log(computerPoints);
+}
+
+function getComputerChoice()
+{
+    randomNumber=Math.random();
+
+    if(randomNumber<0.33)
+    {
+        computerChoice= "rock";
+    }
+    else if(randomNumber>=0.33 && randomNumber<0.66)
+    {
+        computerChoice= "paper";
+    }
+    else
+    {
+        computerChoice= "scissors";
+    }
+}
+
+
+function gameOver()
+{
+    if(playerPoints===5)
+    {
+        console.log("YOU WON THE GAME!!!");
+        return;
+    }
+    else if(computerPoints===5)
+    {
+        console.log("YOU JUST LOST THE GAME.");
+        return;
+    }
+    playRound()
+}
